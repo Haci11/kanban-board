6ffd7c1eef47996dcef46cd21a0ee75862d52a23
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-01-24 11:20:55 +0100

    Made board more dynamic and added colors

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -7,32 +7,19 @@ const Board = () => {
   const { currentProject } = useProject();
   return (
     <div className="board">
-      <div className="board-column">
-        <div className="board-title">
-          <div className="board-title-icon"></div>
-          <h4>Todo</h4>
-        </div>
-        <Ticket
-          item={{
-            id: "1",
-            title: "This is a ticket",
-            descrption: "this is a description",
-            tasks: ["hoppa 3 gånger", "Rasta hunden"],
-          }}
-        />
-      </div>
-      <div className="board-column">
-        <div className="board-title">
-          <div className="board-title-icon"></div>
-          <h4>Doing</h4>
-        </div>
-      </div>
-      <div className="board-column">
-        <div className="board-title">
-          <div className="board-title-icon"></div>
-          <h4>Done</h4>
-        </div>
-      </div>
+      {currentProject.board.map((column) => {
+        return (
+          <div key={column.name} className="board-column">
+            <div className="board-title">
+              <div className={"board-title-icon " + column.name}></div>
+              <h4>{column.name}</h4>
+            </div>
+            {column.tickets.map((ticket) => {
+              return <Ticket key={ticket.id} item={ticket} />;
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
